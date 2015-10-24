b326ac94ec1f356fe149e293d3efdf909b91f319
zhou rui <wirelesser@hotmail.com>
2015-10-25 00:06:25 +0800

    backtest

--- stage1/ms_backtest.py
+++ stage1/ms_backtest.py
@@ -5,26 +5,48 @@ class Ordermg:
     def beginTrade(self):
         self.position = ""
         self.power = 10000.
+        self.gainloss = 0.
         self.shares = 0.
         self.pendingOrder = ""
-        
+        self.totalTrade = 0
+        self.win = 0
+
     def endTrade(self,row):
         avgpx = (row['High']+row['Low']+row['Close']+row['Open'])/4
-        print self.shares * avgpx + self.power
-                
-    def procPendingOrder(self,row):  
+        self.closeTrade(float(avgpx))        
+        #print self.shares * avgpx + self.power
+        print self.totalTrade,float(self.win)/self.totalTrade, self.gainloss, self.gainloss/self.power
+        
+    def closeTrade(self,avgpx):
+        gl = self.shares * avgpx - self.power
+        self.totalTrade += 1
+        #print gl
+        if gl>0:
+            self.win += 1
+        self.gainloss += gl
+        self.shares = 0 
+                    
+    def procPendingOrder(self,row,index):  
         #print row      
         if (self.pendingOrder != ""):
             avgpx = (row['High']+row['Low']+row['Close']+row['Open'])/4
+            #print avgpx
             if (self.pendingOrder == "buy"):
-                #print "buy"
+                print "buy@",index
                 self.shares = self.power / avgpx
-                self.power = 0
                 self.position = "buy"
             elif (self.pendingOrder == "sell"):
-                self.power = self.shares * avgpx
+                '''
+                gainloss = self.shares * avgpx - self.power
+                self.totalTrade += 1
+                if (gainloss>0):
+                    self.win += 1
+                self.gainloss += gainloss
                 self.shares = 0 
-                self.position = "sell"                
+                '''
+                print "sell@",index
+                self.closeTrade(float(avgpx))
+                self.position = "sell"             
             self.pendingOrder = ""   
         pass
         
@@ -34,7 +56,7 @@ class Ordermg:
         pass  
         
     def sellorder(self):
-        if (self.position != "buy"):
+        if (self.position == "buy"):
             self.pendingOrder = "sell"
         pass
     
@@ -43,11 +65,12 @@ class ms_backtest:
         print ohlc
         ordermg = Ordermg()
         ordermg.beginTrade()
+        print "==============================="
         for row_index, row in ohlc.iterrows():
-            ordermg.procPendingOrder(row)
+            ordermg.procPendingOrder(row,row_index)
             if row['buy'] =="buy":
                 ordermg.buyorder()
-            elif row['buy'] =="sell":
+            elif row['sell'] =="sell":
                 ordermg.sellorder()
         lastRow = ohlc.tail(1)
         ordermg.endTrade(lastRow)
