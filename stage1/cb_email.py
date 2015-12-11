6e15350aff6c01b115863f1e195454cc48ba083b
softarts <wirelesser@hotmail.com>
2015-12-11 14:24:22 +0800

    aws update

--- stage1/cb_email.py
+++ stage1/cb_email.py
@@ -7,8 +7,41 @@ from email import encoders
 import mimetypes
 #import sys
 
+def send_gmail(attachment,filedate):
+    test_str="CB_daily_report_"+filedate
+    serverAddr = "smtp.gmail.com"
+    me="wirelesser@gmail.com"
+    me_password="Cv60faqq"
+    you="wirelesser@gmail.com"
+    msg = MIMEMultipart()
+    msg['Subject'] = test_str
+    msg['From'] = me
+    msg['To'] = you
+    msg.preamble = test_str
+    ctype, encoding = mimetypes.guess_type(attachment)
+    maintype, subtype = ctype.split('/', 1)
+    fp = open(attachment, 'rb')
+    msg_pdf = MIMEBase(maintype, subtype)
+    msg_pdf.set_payload(fp.read())
+    fp.close()
+    # Encode the payload using Base64
+    encoders.encode_base64(msg_pdf)
+    # Set the filename parameter
+    msg_pdf.add_header('Content-Disposition', 'attachment', filename=attachment)
+    msg.attach(msg_pdf)
+    smtp_conn = smtplib.SMTP(serverAddr)
+    #smtp_conn.set_debuglevel(1)
+    #smtp_conn.ehlo()
+    smtp_conn.starttls()
+    smtp_conn.ehlo_or_helo_if_needed()
+    smtp_conn.login(me, me_password)
+    smtp_conn.sendmail(me, you, msg.as_string())
+    smtp_conn.quit()
 
 def send_mail(attachment,filedate):
+    send_gmail(attachment,filedate)
+    
+def send_mail0(attachment,filedate):
     test_str="CB_daily_report_"+filedate
     #me="wirelesser@hotmail.com"
     serverAddr = "smtp.gmail.com:587"
@@ -71,4 +104,4 @@ def send_mail(attachment,filedate):
     smtp_conn.quit()
 
 if __name__ == "__main__":
-    send_mail("../cache/dailyreport_2015-12-08.pdf")
+    send_gmail("../cache/dailyreport_2015-12-11.pdf","2015-12-11")
