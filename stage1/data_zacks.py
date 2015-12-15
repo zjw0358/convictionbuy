5e5f1895344851324952d2924aa03dfa8eaf490f
zhou rui <wirelesser@hotmail.com>
2015-12-16 00:08:10 +0800

    earning date

--- stage1/data_zacks.py
+++ stage1/data_zacks.py
@@ -18,6 +18,7 @@ from timeit import default_timer as timer
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 
+import json
 
 class zack_data:
     def __init__(self):
@@ -27,6 +28,7 @@ class zack_data:
         #self.rankPattern = '[\d\D]*Zacks[\D]*Rank[/s]*: (.)[\d\D]*'
 
         self.rankPattern = '[\d\D]*Zacks[\D]*Rank[\s]?: (.)[\d\D]*'
+        self.erdPattern = '^window.app_data_earnings[\d\D]*\\"data\\"[ :\\[]*(.*)]'
         self.cqestCol = ['cq0','cq7','cq30','cq60','cq90']
 
         self.columns = ['rank','indurank','indutotal','etf','abrt','abr1w','abr1m','abr2m',\
@@ -37,15 +39,11 @@ class zack_data:
         self.option = ""
         self.tickdf = pandas.DataFrame()            
         self.mtd = marketdata.MarketData()
-        self.cfg = ms_config.MsDataCfg()  # default = datafile, cb_config.cfg
+        self.cfg = ms_config.MsDataCfg("")  # default = datafile, cb_config.cfg
         self.mkdataFile=self.cfg.getDataConfig("marketdata") #"./marketdata.csv"
-        folder = self.cfg.getDataConfig("cache")
-        self.outputfn = folder + "msdata_zack_" + datetime.datetime.now().strftime("%Y-%m-%d") + '.csv' 
+        self.cachefolder = self.cfg.getDataConfig("folder","../cache/")
+        self.outputfn = self.cachefolder + "msdata_zack_" + datetime.datetime.now().strftime("%Y-%m-%d") + '.csv'         
 
-
-        #self.zackfile = self.cfg.getDataConfig("","zack") #"./marketdata.csv"
-        #print self.mkdataFile
-        #sys.exit()
     def parseOption(self):
         self.ticklist=[]
         try:
@@ -88,46 +86,57 @@ class zack_data:
         print "run zack_data.py -r zackfile -t aapl --merge" #merge with zackfile
         print "run zack_data.py -i 1,2,3 "
 
-
-
-    '''    
-    def getRank(self,ticklist):
-        zackranks = {}
-        for symbol in ticklist:
-            url = "http://www.zacks.com/stock/quote/"+symbol
-            htmltxt =urllib2.urlopen(url).read()
-            an = re.match(self.rankPattern, htmltxt)
+    def parseJson(self,content):
+        #remove \"
+        #content = re.sub("\\", 'abcd', content)
+        content = '['+content.replace('\\\"', '')+']'
+        #print content        
+        outer = json.loads(content)
+        print outer[0]['Date']
+        #inner = json.dumps(outer[0])
+    #get earning date
+    def getERD(self,symbol):
+        url = "http://www.zacks.com/stock/research/"+symbol+"/earnings-announcements"
+        page =urllib2.urlopen(url)
+        soup = BeautifulSoup(page.read())
+        allitems = soup.findAll('script')
+        for index,item in enumerate(allitems):
+            txt = item.string
+            if txt==None:
+                continue
+            '''else:
+                print index,"=",txt'''
+            an = re.match(self.erdPattern,txt)
             if an!=None:
                 str1=an.group(1)
-                if str1=='N':
-                    zrank = 0
-                else:
-                    zrank = int(str1[0])
-                print symbol, zrank
-                zackranks[symbol] = zrank
-        return zackranks
-    '''
-    '''
-    def getSymbolRank(self,symbol):
-        zrank = -1
-        url = "http://www.zacks.com/stock/quote/"+symbol
-        try:            
-            htmltxt =urllib2.urlopen(url).read()
-        except:
-            print symbol," Not found"
-            return zrank
-        an = re.match(self.rankPattern, htmltxt)
-        if an!=None:
-            str1=an.group(1)
-            if str1=='N':
-                zrank = 0
-            else:
-                zrank = int(str1[0])
-        print symbol, zrank    
-        return zrank
-    '''
+                erlst=[]
+                #print str1
+                self.parseJson(str1)
+                '''
+                for pairstr in str1.split(','):
+                    name,value = pairstr.split(':')
+                    name=re.sub('[{} "]','',name)
+                    #print name,value
+                    if name=='Date':
+                        value=re.sub('[ "]','',value)
+                        #d = datetime.datetime.strptime(value, '%m/%d/%Y')
+                        erlst.append(value)
+                self.saveErdFile(symbol,erlst)
+                '''
+                return
+        print "\tEarning date not found"
+                #print erlst 
+                    
+    def saveErdFile(self,symbol,erlst):
+        fileName=self.cachefolder+symbol+"_erdate.erd"
+        #print "saveErdFile",fileName
+        fp = open(fileName,'w',-1)
+        #pickle.dump(erlst, fp)
+        #fp.writelines(erlst)
+        fp.writelines(["%s\n" % item  for item in erlst])
+        fp.close()
   
-        
+    #obselete  
     def parseRank(self,htmltxt):
         an = re.match(self.rankPattern, htmltxt)
         if an!=None:
@@ -221,8 +230,6 @@ class zack_data:
         #print symbol,abr
         return abr
         
-   
-        
     #first try             
     def getEstimate(self, symbol):
         start = timer()
@@ -286,9 +293,7 @@ class zack_data:
         for key in self.columns:
             dct[key]=""
         #dct['etf']="0"
-
-
-
+        
         estdct = self.getEstimate(symbol)
         if self.isEtf:
             return None
@@ -303,9 +308,13 @@ class zack_data:
             dct.update(abrdct)
         end = timer()    
         print "\tbroker recom",(end-start) 
+
+        start = timer()
+        self.getERD(symbol)
+        end = timer()    
+        print "\tearning date",(end-start) 
+
         return dct
-                
-   
   
     #load zack csv file
     def loadZackCsvFile(self,fileName):
@@ -443,11 +452,8 @@ class zack_data:
 ################################################################################            
 if __name__ == "__main__":
     obj = zack_data()
-    obj.process()
-    #zr.getEstimate('intc')
-    #zr.getBrokerRecom('intc')
-    #zr.getPriceSale('intc')
     #obj.process()
-
+    obj.getERD('MGM')    
+    
 
 
